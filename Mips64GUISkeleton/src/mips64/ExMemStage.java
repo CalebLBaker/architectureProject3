16b56dfd475efa090dcbbfe8eb611f32540cbbd2
mjentner <mattentner@cedarville.edu>
2018-10-30 20:44:15 -0400

    Main file

--- Mips64GUISkeleton/src/mips64/ExMemStage.java
+++ Mips64GUISkeleton/src/mips64/ExMemStage.java
@@ -3,6 +3,7 @@ package mips64;
 public class ExMemStage {
 
     PipelineSimulator simulator;
+    boolean halted;
     boolean shouldWriteback = false;
     int instPC;
     int opcode;
@@ -13,12 +14,42 @@ public class ExMemStage {
     boolean memRead;
     boolean memWrite;
     boolean branchTaken;
-    boolean wbSource;
+    boolean isLoad;
 
     public ExMemStage(PipelineSimulator sim) {
         simulator = sim;
     }
 
     public void update() {
+    }
+
+    boolean getHalted() {
+        return halted;
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    boolean getshouldWriteback() {
+        return shouldWriteback;
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    int getinstPC() {
+        return instPC;
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    int getAluIntData() {
+        return aluIntData;
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    int getDestReg() {
+        return destReg;
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    boolean getWbSource() {
+        return isLoad;
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }

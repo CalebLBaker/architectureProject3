957aaf03e4ef78f84540e165d83d0adcc5f63dd8
mjentner <mattentner@cedarville.edu>
2018-10-30 20:15:13 -0400

    Main file

--- Mips64GUISkeleton/src/mips64/MemWbStage.java
+++ Mips64GUISkeleton/src/mips64/MemWbStage.java
@@ -4,11 +4,14 @@ public class MemWbStage {
 
     PipelineSimulator simulator;
     boolean halted;
-    boolean shouldWriteback = false;
+    boolean shouldWriteback = false; //doesWB
     int instPC;
     int opcode;
-    int aluIntData;
-    int loadIntData;
+    int aluIntData; //aluResult
+    int loadIntData;//mem result
+    int DestReg; //DestinationReg
+    int wbSource; //ALU or memory
+    
 
     public MemWbStage(PipelineSimulator sim) {
         simulator = sim;
